f5e95f35a7bf37280189523b7474bb6ebc8a854c
Jan Engelhardt <jengelh@inai.de>
2016-04-22 22:50:42 +0200

    xt_pknock: replace nemesis by socat
    Use a utility much more widely available.

--- extensions/pknock/knock.sh
+++ extensions/pknock/knock.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-# $1 -> IP src
-# $2 -> IP dst
-# $3 -> PORT dst
-# $4 -> secret
-
-if [ -z $4 ]; then 
+if [ "$#" -ne 4 ]; then
     echo "usage: $0 <IP src> <IP dst> <PORT dst> <secret>"
     exit 1
 fi
-
-digest_file="/tmp/digest.txt"
-
-python ../test/py/gen_hmac.py $4 $1 > $digest_file
-nemesis udp -S $1 -D $2 -y $3 -P $digest_file
+python gen_hmac.py "$4" "$1" | socat - "udp-sendto:$2:$3,bind=$1"
