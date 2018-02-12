4de9ff1505f4d45e792743d92d55014f64c1d56a
Jan Engelhardt <jengelh@inai.de>
2018-02-12 14:48:34 +0100

    build: remove support for Linux 3.16

--- extensions/compat_xtables.h
+++ extensions/compat_xtables.h
@@ -8,8 +8,8 @@
 
 #define DEBUGP Use__pr_debug__instead
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 16, 0)
-#	warning Kernels below 3.16 not supported.
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 17, 0)
+#	warning Kernels below 3.17 not supported.
 #endif
 
 #if defined(CONFIG_NF_CONNTRACK) || defined(CONFIG_NF_CONNTRACK_MODULE)
