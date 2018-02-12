3a24468a6858a20f61ace75338545b52872f9e53
Jan Engelhardt <jengelh@inai.de>
2018-02-12 14:56:05 +0100

    build: remove support for Linux 4.4

--- extensions/compat_xtables.h
+++ extensions/compat_xtables.h
@@ -8,8 +8,8 @@
 
 #define DEBUGP Use__pr_debug__instead
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 0)
-#	warning Kernels below 4.4 not supported.
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 5, 0)
+#	warning Kernels below 4.5 not supported.
 #endif
 
 #if defined(CONFIG_NF_CONNTRACK) || defined(CONFIG_NF_CONNTRACK_MODULE)
